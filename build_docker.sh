600ee3cbad9d81350e986fc54a56353445b7c23e
Jeferson Guardezi <guardezi@siliconvillage.com.br>
2022-03-14 16:09:40 -0300

    initial commit

---
+++ build_docker.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+set -e
+
+docker build --cache-from cirrusci/flutter:${FLUTTER_VERSION/+/-} \
+             --tag cirrusci/flutter:${FLUTTER_VERSION/+/-} \
+             --tag cirrusci/flutter:$DOCKER_TAG \
+             --build-arg flutter_version=$FLUTTER_VERSION \
+             sdk
+#docker build --cache-from cirrusci/flutter:${FLUTTER_VERSION/+/-}-web \
+#             --tag cirrusci/flutter:${FLUTTER_VERSION/+/-}-web \
+#             --tag cirrusci/flutter:$DOCKER_TAG-web \
+#             --build-arg flutter_tag=${FLUTTER_VERSION/+/-} \
+#             web
